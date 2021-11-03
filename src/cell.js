be19a476fb1bd38ace3c3e85354eba61e34865c1
parvinfrzd <parvin.farahzadeh@gmail.com>
2021-11-02 22:42:58 -0400

    make grid and bees

---
+++ src/cell.js
@@ -0,0 +1,7 @@
+//make a grid with  cells  and bees
+
+// click on cell to reveal the status of cell
+
+// check win or lose 
+//a. if a cell state is bee then you lose 
+//b. if a cell state is not bee then you go on 
--- src/script.js
+++ src/script.js
@@ -1,17 +1,89 @@
-$(document).ready(function () {
-    function MakeColumn(column) {
-        for (let i = 0; i < column; i++) {
-            //make columns and append them to container 
-            $("<div class='column'/>").appendTo(".container");
-        }
+//make a grid with  cells  and bees
+
+// click on cell to reveal the status of cell
+
+// check win or lose 
+//a. if a cell state is bee then you lose 
+//b. if a cell state is not bee then you go on 
+/*const cell = {
+    DOM element, 
+    x, 
+    y, 
+    STATUS
+
+}*/
+
+window.onload = function () {
+    const status = {
+        HIDDEN: 'hidden',
+        REVEALED: 'revealed',
     }
-    MakeColumn(10);
-    MakeRow(10);
-    function MakeRow(row) {
-        columns = $('.column');
-        console.log(columns);
-        for (let j = 0; j < row; j++) {
-            $("<button class='row'/>").appendTo('.column');
+    const gridCount = 3;
+    const bees = 3;
+    const beeGrid = MakeGridAndBees(gridCount);
+    MakeBees(beeGrid, bees);
+
+    function MakeGridAndBees(size) {
+        grid = []
+        gridEl = document.querySelector('.grid');
+        for (let i = 0; i < size; i++) {
+            //make columns  
+            column = new Array(size);
+            for (let j = 0; j < size; j++) {
+                //make div and display
+                el = document.createElement('div');
+                el.setAttribute('class', 'cell');
+                gridEl.appendChild(el);
+
+                //construct cell 
+                const cell = {
+                    el,
+                    x: i,
+                    y: j,
+                    isBee: false,
+                    state: status.HIDDEN
+                }
+                column.push(cell);
+            }
+            grid.push(column);
         }
+        return grid;
+    };
+
+    function MakeBees(grid, beeCount) {
+        randomArr = generateRandom(beeCount)
+        grid.forEach(column => column.forEach(function (cell) {
+            randomArr.forEach(function (random) {
+                if (checkeNumberMatch(cell, random)) {
+                    cell.isBee = true;
+                    cell.el.style.backgroundColor = 'red';
+                }
+            });
+        }));
+        console.log(grid);
+        return grid;
     }
-});
+
+    //function to generate random x and y
+    function generateRandom(beeCount) {
+        let generatedNumbers = [];
+        for (let i = 0; i < beeCount; i++) {
+            const index = {
+                x: Math.floor(Math.random() * beeCount),
+                y: Math.floor(Math.random() * beeCount)
+            }
+            //doesn't work!
+            if (!generatedNumbers.includes(index)) {
+                generatedNumbers.push(index);
+            }
+        }
+        console.log(generatedNumbers);
+        return generatedNumbers;
+    }
+    //function to see if numbers match with generated numbers 
+    function checkeNumberMatch(a, b) {
+        return a.x === b.x && a.y === b.y;
+    }
+
+
+}
