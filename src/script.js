bc66a68e753cf2a45c28a3681ce1bc80f844368a
parvinfrzd <parvin.farahzadeh@gmail.com>
2021-11-03 22:25:22 -0400

    make neighbour array

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
-//make a grid with  cells  and bees
-
-// click on cell to reveal the status of cell
+// click on cell to reveal the styleStatus of cell
 
 // check win or lose 
 //a. if a cell state is bee then you lose 
@@ -10,7 +8,7 @@
     DOM element, 
     x, 
     y, 
-    STATUS
+    styleStatus
 
 }*/
 const gridCount = 3;
@@ -18,21 +16,67 @@ const bees = 3;
 
 
 window.onload = function () {
-    const status = {
-        HIDDEN: 'hidden',
-        REVEALED: 'revealed',
+    const styleStatus = {
+        EMPTY: 'empty',
+        NEIGHBORED: 'neighbored',
+        BOMB: 'bomb'
     }
 
-    const beeGrid = MakeGridAndBees(gridCount);
-    MakeBees(beeGrid, bees);
+    const beeGrid = MakeGrid(gridCount);
+    MakeBees(beeGrid, bees, gridCount);
+
+    beeGrid.forEach(column => column.forEach(function (cell) {
+        console.log(FindNeighborCells(cell, gridCount));
+    }))
+
+    console.log(MakeNeighborArray(beeGrid[0][0], gridCount));
 
 
-    function MakeGridAndBees(size) {
-        grid = []
+    function MakeNeighborArray(cell, size) {
+        neighbours = [
+            { x: cell.x + 1, y: cell.y + 1 },
+            { x: cell.x + 1, y: cell.y - 1 },
+            { x: cell.x + 1, y: cell.y },
+            { x: cell.x - 1, y: cell.y - 1 },
+            { x: cell.x - 1, y: cell.y + 1 },
+            { x: cell.x - 1, y: cell.y },
+            { x: cell.x, y: cell.y + 1 },
+            { x: cell.x, y: cell.y - 1 }
+        ];
+        //filter the ones that are not in the grid. 
+        let count = neighbours.length;
+        while (count--)
+            if (neighbours[count].x < 0 || neighbours[count].y < 0 || neighbours[count].x > size - 1 || neighbours[count].y > size - 1) {
+                const index = neighbours.indexOf(neighbours[count]);
+                neighbours.splice(index, 1);
+            }
+        return neighbours;
+    }
+
+    function FindNeighborCells(cell, size) {
+        neighbourCells = [];
+        neighbours = MakeNeighborArray(cell, size);
+        neighbours.forEach(function (neighbour) {
+            if (checkeNumberMatch(neighbour, cell)) {
+                neighbourCells.push(cell);
+            } else {
+                console.log('doesnt match', neighbour)
+                console.log('the cell is:', cell)
+            }
+        });
+        return neighbourCells
+    }
+
+    function ClickToGo() {
+        //click to reveal 
+    }
+
+    function MakeGrid(size) {
+        grid = new Array()
         gridEl = document.querySelector('.grid');
         for (let i = 0; i < size; i++) {
             //make columns  
-            column = new Array(size);
+            column = new Array();
             for (let j = 0; j < size; j++) {
                 //make div and display
                 el = document.createElement('div');
@@ -45,7 +89,7 @@ window.onload = function () {
                     x: i,
                     y: j,
                     isBee: false,
-                    state: status.HIDDEN
+                    state: styleStatus.HIDDEN
                 }
                 column.push(cell);
             }
@@ -54,27 +98,27 @@ window.onload = function () {
         return grid;
     };
 
-    function MakeBees(grid, beeCount) {
-        randomArr = generateRandom(beeCount)
+    function MakeBees(grid, beeCount, size) {
+        randomArr = generateRandom(beeCount, size)
         grid.forEach(column => column.forEach(function (cell) {
             randomArr.forEach(function (random) {
                 if (checkeNumberMatch(cell, random)) {
                     cell.isBee = true;
-                    cell.el.style.backgroundColor = 'red';
+                    cell.state = styleStatus.BOMB;
+                    cell.el.setAttribute('class', `cell ${cell.state}`);
                 }
             });
         }));
-        console.log(grid);
         return grid;
     }
 
     //function to generate random x and y
-    function generateRandom(beeCount) {
+    function generateRandom(beeCount, size) {
         let generatedNumbers = [];
         while (generatedNumbers.length < beeCount) {
             const index = {
-                x: Math.floor(Math.random() * beeCount),
-                y: Math.floor(Math.random() * beeCount)
+                x: Math.floor(Math.random() * size),
+                y: Math.floor(Math.random() * size)
             }
             if (!generatedNumbers.some(number => checkeNumberMatch(number, index)))
                 generatedNumbers.push(index);
